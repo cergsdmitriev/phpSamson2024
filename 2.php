65b7bb2b6bcca5eb04586654f9094d2cdb06db04
Dmitriev Sergey <semidues@gmail.com>
2024-07-29 11:46:42 +0300

    mySortForKey function add

--- 2.php
+++ 2.php
@@ -18,5 +18,50 @@
     // три вхождения, заменяем только второе
     print "<br>Coll convertString(abfabfffab, ab)<br>";
     print_r( convertString("abfabfffab", "ab"));
+
+    // функцию mySortForKey($a, $b). $a – двумерный массив вида [['a'=>2,'b'=>1],['a'=>1,'b'=>
+    // 3]], $b – ключ вложенного массива. Результат ее выполнения: двумерном массива $a
+    // отсортированный по возрастанию значений для ключа $b. В случае отсутствия ключа $b в
+    // одном из вложенных массивов, выбросить ошибку класса Exception с индексом
+    // неправильного массива.
+    function mySortForKey(array &$inpArr, string $sortKey) : array
+    {
+        $sortKeyArr = [];
+        foreach($inpArr as $arrInd => $arrVal){
+            if(isset($arrVal[$sortKey]))
+                array_push($sortKeyArr, $arrVal[$sortKey]);
+            else {
+                throw new Exception("Key '$sortKey' does not exist on array index '$arrInd' !");
+            }
+        }
+        array_multisort($sortKeyArr, $inpArr);
+
+        return $inpArr;
+    }
     
+    //тестирование
+    $testMySortForKey = [['a'=> 2, 'b' => 45, 'c' => 10],['a'=> 10, 'b' => 1],['a'=> 7, 'b' => 25, 'c' => 10],['a'=> 5, 'b' => 50, 'c' => 10]];
+    print "<br> Test array mySortForKey() ";
+    foreach($testMySortForKey as $testVal){
+        print "<br>";
+        print_r($testVal);
+    }
+    print "<br> After sort by 'b'<br>";
+    mySortForKey($testMySortForKey, 'b');
+    foreach($testMySortForKey as $testVal){
+        print "<br>";
+        print_r($testVal);
+    }
+    print "<br> After sort by 'a'<br>";
+    mySortForKey($testMySortForKey, 'a');
+    foreach($testMySortForKey as $testVal){
+        print "<br>";
+        print_r($testVal);
+    }
+
+    // print "<br> After sort by 'c'<br>";
+    // mySortForKey($testMySortForKey, 'c');
+
+    
+
 ?>
